e7843cdddd12c46e77024bb99fdcdc763e2995b7
noeun0 <noeun_y@naver.com>
2021-08-15 23:01:08 +0900

    0815 upload

--- swea/4831_전기버스/s1.py
+++ swea/4831_전기버스/s1.py
@@ -1,49 +1,14 @@
-'''
-3
-3 10 5
-1 3 5 7 9
-3 10 5
-1 3 7 8 9
-5 20 5
-4 7 9 14 17
-'''
-import sys
-sys.stdin = open('input.txt')
-case = int(input())
-i = 0
-p = 0
-m_stop = list()
-
-listx = list()
-
-
-def check_f(x):
-    ans = 0
-    # 지나는 정류장을 임의로 담아줌
-    for m in stop:
-
-        if x < m <= (x + k):
-            ans = m  # m이 순차적으로 돌면서 저장저장 최댓값 저장됨
-    if ans == 0:
-        del m_stop[:]
-        return
-
-    else:
-        m_stop.append(ans)  # m이 추가된다.
-
-    x = ans
-    if x >= (n - k):
-        return
-    else:
-        check_f(x)
-
-
-for i in range(case):
-    p = i
-    k, n, m = map(int, input().split())  # 최대 정류장 수, 정류장 수, 충전기가 설치된 정류장의 개수
-    stop = list(map(int, input().split()))
-    x = 0
-    m_stop = []
-    check_f(x)
-
-    print(f"#{p + 1} {len(m_stop)}")
+for tc in range(1,int(input())+1):
+        K,N,M = map(int,input().split())
+        stoplist = list(map(int,input().split()))       
+        stoplist.sort(reverse=True)
+        pre = 0
+        count=0
+        while pre+K<N:
+            if {i for i in range(pre,pre+K+1)} & set(stoplist):
+                pre=stoplist.pop(stoplist.index(max({i for i in range(pre,pre+K+1)} & set(stoplist))))                    
+            else:
+                count=0
+                break
+            count+=1
+        print("#{} {}".format(tc,count))
