a6cebdd11aa71ff310e5a5f712ffb98814971459
noeun0 <noeun_y@naver.com>
2021-10-18 23:57:38 +0900

    1018 upload

--- 백준/17135. 캐슬디펜스/s1.py
+++ 백준/17135. 캐슬디펜스/s1.py
@@ -1,17 +1,41 @@
+from itertools import permutations
 import sys
+
 sys.stdin = open('input.txt')
 
-N,M,D = map(int,input().split()) # 행의 수 , 열의 수, 공격거리 제한
-board = [[i for i in map(int,input().split())] for i in range(N)]
+# 세개를 선택하는 경우
 
-print(board)
+dx = [-1, 0, 0]
+dy = [0, -1, 1]
+
+
+def choice(n, th):
+    global arr
+    if len(th) == 3:
+
+        arr.append(th)
+        return
+    for i in range(M):
+        th.append(i)
+        choice(n + 1, th)
+        th.pop(-1)
+
+
 def attack():
+    # 무조건 앞으로 한칸 가고, 그 후로 왼 오 위
     pass
+
 
 def go():
-    pass
+    board.pop(-1)
 
 
-while board:
-    attack()
+N, M, D = map(int, input().split())  # 행의 수 , 열의 수, 공격거리 제한
+board = [[i for i in map(int, input().split())] for i in range(N)]
+arr = []
+t_list = []
+choice(0, t_list)
+print(arr)
+
+while 1 in board[-1]:
     go()
---
+++ 백준/1931. 회의실배정/s1.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open("input.txt")
+
+
+def func(s,cnt):
+    global result
+    if list_h[s][1] > end or s == N:
+        result.append(cnt)
+        #print(check)
+        return
+
+    for m in range(s,N):
+        if list_h[s][1] <= list_h[m][0]:
+            if cnt >= check[m]:
+                check[m] = cnt+1
+                func(m,cnt+1)
+            else:
+                return
+
+N  = int(input())
+list_h = [[i for i in map(int,input().split())] for i in range(N)]
+list_h = sorted(list_h, key =lambda x : x[0])
+#print(list_h)
+result = []
+check = [0]*N
+end = list_h[-1][0]
+for i in range(2):
+    start = list_h[i][0]
+    func(i,1)
+
+print(max(result))
